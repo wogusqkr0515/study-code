e764feabaf259bb05bff84529a3e6be2c87baefb
wogusqkr0515 <74415324+wogusqkr0515@users.noreply.github.com>
2021-02-04 01:56:22 +0900

    study
    gcd(1,num)해서 1 나오면  된다 생각했는데 잘못 생각함

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-a = [1, 2,2,1,2]
-b = []
-b.append(list(i for i, ele in enumerate(a) if ele == 2))
-print(b)
+from math import gcd
+
+print(gcd(1,39))
--- 백준 알고리즘/sw역량 테스트 준비-기초/수학/6588.py
+++ 백준 알고리즘/sw역량 테스트 준비-기초/수학/6588.py
@@ -1,28 +1,58 @@
 ##  골드바흐의 추측  ##
+import math
+from math import gcd
 
 num = []
 while True :
-    if 0 in num :
-        break
     n = int(input())
+    if n == 0 :
+        break
     num.append(n)
-lenNum = len(num) - 1
+
+lenNum = len(num)
 plus = [[]*lenNum for i in range(lenNum)]
 
-for i in range(lenNum) :
-    x,k, yesNo = 0,3, False
+# for i in range(lenNum) :
+#     x,k, yesNo = 0,3, False
+#     while True :
+#         x = num[i] - k
+#         for z in range(2,x+1) :
+#             if z == x :
+#                 yesNo = True
+#             elif x % z == 0 :
+#                 break
+#         if yesNo == True :
+#             break
+#         k += 2
+#     plus[i].append(k)
+#     plus[i].append(x)
+
+for i in range(lenNum) :              ## 잘못 생각함 
+    num1,num2,box,yesNo = 3,0,0,False
     while True :
-        x = num[i] - k
-        for z in range(2,x+1) :
-            if z == x :
-                yesNo = True
-            elif x % z == 0 :
-                break
+        box = num[i] - num1
+        if gcd(num1,box) == 1 :
+            num2 = box
+            yesNo = True
+        # print('1== ',box)
+        # sqr = int(math.sqrt(box))+1
+        # print('2 == ',sqr)
+        # for k in range(2,sqr) :
+        #     if box % k == 0 :
+        #         num2 = box
+        #         yesNo = True
+        #         break
         if yesNo == True :
+            plus[i].append(num1)
+            plus[i].append(num2)
             break
-        k += 2
-    plus[i].append(k)
-    plus[i].append(x)
+        else :
+            num1 += 2
+            if gcd(1,num1) != 1 :
+                while gcd(1,num1) != 1 :
+                    num1 += 1
+
+
 
 for i in range(lenNum) :
     print(num[i], end=' = ')
