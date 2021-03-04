608a182fe82eb6361da4aa5f8b67667cd45da621
wogusqkr0515 <74415324+wogusqkr0515@users.noreply.github.com>
2021-03-04 22:58:27 +0900

    study

---
+++ boj/1931.py
@@ -0,0 +1,14 @@
+# n = int(input())
+# time = []
+# timeIndex = []
+  
+# for i in range(n) :
+#     # start, end = map(int,input().split())
+#     # time[i].append(start)
+#     # time[i].append(end)
+#     a = list(map(int,input().split()))
+#     time.append(a)
+# time.sort()
+# box = 0
+
+print('aaa')
